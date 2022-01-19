53ced609ea0845a25d7f770cd453bd0f61278de7
zzhilin <zhilin_li@outlook.com>
2022-01-19 15:22:15 -0700

    Added prework files

---
+++ Codepath Prework Calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Codepath Prework Calculator
+//
+//  Created by Zhilin Li on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        // Get total tip
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
